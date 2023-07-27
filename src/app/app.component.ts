8c1e5730b267c800485d76557ee5e2655b083603
Indraprasath <indraprasathmanickavel@gmail.com>
2023-07-27 17:59:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directive';
+
+  products =[
+    {name:"Iphone", model:1},
+    {name:"Android", model:2}
+  ];
+
+  pro =[
+    {name:"Iphone", model:1, status:"Available"},
+    {name:"Android", model:2, status:"Not Available"},
+    {name:"Linux", model:1, status:"Available"},
+    {name:"Mac", model:2, status:"Not Available"}
+  ];
+
+  svalue: string='';
+  searchvalue(eventData:any){
+    this.svalue=(<HTMLInputElement>eventData.target).value;
+  }
+  
 }
